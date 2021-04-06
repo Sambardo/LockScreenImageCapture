f4ff17f78066bec7d3c87b7d07a9320cd6597462
Kory T <korythacher@gmail.com>
2021-04-06 07:03:36 -0700

    adding initial script

---
+++ Get-LockScreenImage.ps1
@@ -0,0 +1,7 @@
+﻿param($SavePath = "~\Desktop\LockScreen$((Get-Date).ToString("dd-MM-yyyy")).jpg" )
+
+$SID = [Security.Principal.WindowsIdentity]::GetCurrent().User.value
+$Key = "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Creative\"
+$Item = (dir "$Key\$SID")[-1]
+$Path = $Item.GetValue("landscapeImage")
+Copy-Item -Path $Path -Destination "$SavePath"
