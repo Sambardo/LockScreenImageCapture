eb7a4d831af054df6352fe4fc9b353c645786b1d
Kory T <korythacher@gmail.com>
2021-05-06 12:38:51 -0700

    test

---
+++ GUI.ps1
@@ -0,0 +1,36 @@
+#region Load GUI Elements
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+
+#Cleanup XAML options
+$XAML= [XML](Get-Content -Path "$PSScriptRoot\MainWindow.xaml" -Raw)
+$XAML.Window.RemoveAttribute('x:Class')
+$XAML.Window.RemoveAttribute('xmlns:local')
+$XAML.Window.RemoveAttribute('xmlns:d')
+$XAML.Window.RemoveAttribute('xmlns:mc')
+$XAML.Window.RemoveAttribute('mc:Ignorable')
+
+#read XML as XAML
+$XAMLreader = New-Object System.Xml.XmlNodeReader $XAML
+$Rawform = [Windows.Markup.XamlReader]::Load($XAMLreader) 
+
+#add XML namespace manager
+$XmlNamespaceManager = [System.Xml.XmlNamespaceManager]::New($XAML.NameTable)
+$XmlNamespaceManager.AddNamespace('x','http://schemas.microsoft.com/winfx/2006/xaml')
+
+#Create hash table containing a representation of all controls
+$GUI = @{}
+$namedNodes = $XAML.SelectNodes("//*[@x:Name]",$XmlNamespaceManager)
+$namedNodes | ForEach-Object -Process {$GUI.Add($_.Name, $Rawform.FindName($_.Name))}
+#endregion Load GUI Elements
+
+#region button code
+$ButtonCode = {
+    &"$PSScriptRoot\Get-LockScreenImage.ps1" -SavePath "$PSScriptRoot\Image.jpg"
+    $GUI["ImageBox"].Source = "$PSScriptRoot\Image.jpg" 
+}
+$GUI["FetchButton"].add_Click($ButtonCode)
+
+#endregion button code
+
+#Show GUI
+$Rawform.ShowDialog() 
